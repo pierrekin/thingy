051ed15a11e472e554f670fa20ccfa1617c945b7
Pierre Hugo <pierrekin@househugo.com>
2026-03-28 19:00:30 +0200

    chore: use r2 bucket for mantle binaries

--- packages/mantle/src/cli.ts
+++ packages/mantle/src/cli.ts
@@ -114,7 +114,7 @@ const standalone = defineCommand({
 export const main = defineCommand({
 	meta: {
 		name: "mantle",
-		version: "0.0.3",
+		version: "0.0.4",
 		description: "Mantle CLI",
 	},
 	subCommands: {
